ef5bdf66f4942569b1b4a29b8cbbc02627dd060e
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2024-07-01 01:42:49 -0300

    Init

---
+++ src/mocks/carros.js
@@ -0,0 +1,39 @@
+export const carros = [
+    {
+        id: 1,
+        modelo: 'Gol',
+        marca: 'Volkswagen',
+        categoria: 'Popular',
+        ano: 2020,
+        diaria: 100,
+        foto: 'https://cdn.autopapo.com.br/carro/volkswagen/gol-16-msi-comfortline-flex-2017/destaque-v1.png'
+    },
+    {
+        id: 2,
+        modelo: 'Onix',
+        marca: 'Chevrolet',
+        categoria: 'Popular',
+        ano: 2020,
+        diaria: 110,
+        foto: 'https://orca.com.br/uploads/products/versions/novo-onix-rs-turbo-2021-vermelho.png'
+    },
+    {
+        id: 3,
+        modelo: 'Corolla',
+        marca: 'Toyota',
+        categoria: 'Executivo',
+        ano: 2020,
+        diaria: 150,
+        foto: 'https://www.toyotacomunica.com.br/wp-content/uploads/2023/09/pagina-24_COROLLA-2024_GR-S_2.png'
+    },
+    {
+        id: 4,
+        modelo: 'Civic',
+        marca: 'Honda',
+        categoria: 'Executivo',
+        ano: 2020,
+        diaria: 160,
+        foto: 'https://www.pngmart.com/files/4/Honda-Civic-PNG-Transparent-Picture.png'
+    }
+]
+        
